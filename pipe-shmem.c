b34b6361bf1fdbdb7ce2c609d64ea08f7af6e1e9
Atanas Yordanov Shalaverov <ashalaverov@mm-sol.com>
2019-07-08 14:50:05 +0300

    Created shared memory reader
    Successfully reading data from shared memory segment

---
+++ pipe-shmem.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+void handle_error(int err_code)
+{
+	fprintf(stderr, "Got error %s\n", strerror(err_code));
+	exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[])
+{
+	int shared_mem_id = 0;
+	char* shared_mem_message;
+	struct shmid_ds shared_mem_buffer;
+	const int shared_segment_size = 0x400;
+
+	if(argc < 2) {
+		fprintf(stderr, "Usage: ./pipe-shmem shared-memory-id\n");
+		exit(EXIT_FAILURE);
+	} else {
+		shared_mem_id = atoi(argv[1]);
+		if(shared_mem_id == 0) {
+			fprintf(stderr, "Could not convert %s to number\n", argv[1]);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	printf("%d\n", shared_mem_id);
+
+	shared_mem_message = (char*) shmat(shared_mem_id, NULL, 0);
+
+	if(shared_mem_message == (void*) -1) {
+		handle_error(errno);
+	}
+
+	printf("Got message: %s", shared_mem_message);
+
+	if(shmdt(shared_mem_message) == -1) {
+		handle_error(errno);
+	}
+	
+	if(shmctl(shared_mem_id, IPC_RMID, &shared_mem_buffer) == -1) {
+		handle_error(errno);
+	}
+
+	return 0;
+}
